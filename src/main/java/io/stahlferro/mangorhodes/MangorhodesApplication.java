462db0ead8cf62388696c3627b6d61c61cf49a4e
StahlFerro <StahlFerro@users.noreply.github.com>
2020-09-29 13:27:28 +0700

    First commit

---
+++ src/main/java/io/stahlferro/mangorhodes/MangorhodesApplication.java
@@ -0,0 +1,13 @@
+package io.stahlferro.mangorhodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangorhodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangorhodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/stahlferro/mangorhodes/MangorhodesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stahlferro.mangorhodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangorhodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
