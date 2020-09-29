043d0911e2c6e290aded7601e11bbad7292d7c69
StahlFerro <StahlFerro@users.noreply.github.com>
2020-09-29 21:42:58 +0700

    Added creation and modification time stamps; Utilize deserialization for localdateoptionaltime on Operator and Mission

--- src/main/java/io/stahlferro/mangorhodes/models/primary/Operator.java
+++ src/main/java/io/stahlferro/mangorhodes/models/primary/Operator.java
@@ -1,16 +1,22 @@
 package io.stahlferro.mangorhodes.models.primary;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.stahlferro.mangorhodes.deserializers.LocalDateOptionalTimeDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -26,4 +32,10 @@ public class Operator {
     private String code;
     private String codeName;
     private int accessLevel;
+    @JsonDeserialize(using = LocalDateOptionalTimeDeserializer.class)
+    private LocalDateTime hireDate;
+    @CreationTimestamp
+    private Timestamp creationTimeStamp;
+    @UpdateTimestamp
+    private Timestamp modificationTimeStamp;
 }
--- src/main/java/io/stahlferro/mangorhodes/models/secondary/Mission.java
+++ src/main/java/io/stahlferro/mangorhodes/models/secondary/Mission.java
@@ -1,14 +1,20 @@
 package io.stahlferro.mangorhodes.models.secondary;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.stahlferro.mangorhodes.deserializers.LocalDateOptionalTimeDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -22,6 +28,13 @@ public class Mission {
     private String type;
     private long reward;
     private ArrayList<String> operatorCodes;
+    @JsonDeserialize(using = LocalDateOptionalTimeDeserializer.class)
+    private LocalDateTime startDate
+    @CreationTimestamp
+    private Timestamp creationTimeStamp;
+    @UpdateTimestamp
+    private Timestamp modificationTimeStamp;
+
 
     public Mission() {
         this.uuid = UUID.randomUUID();
