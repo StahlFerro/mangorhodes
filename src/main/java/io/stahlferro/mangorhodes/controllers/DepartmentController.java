1fbe8757aa167190d884ad6d16e42bc1e942ee9b
StahlFerro <StahlFerro@users.noreply.github.com>
2020-10-06 21:46:56 +0700

    Added deletion controllers for department and room; Added UnitTest to ensure successful insertion and deletion of department and room

--- src/main/java/io/stahlferro/mangorhodes/controllers/DepartmentController.java
+++ src/main/java/io/stahlferro/mangorhodes/controllers/DepartmentController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/department")
@@ -24,5 +26,13 @@ public class DepartmentController {
     public ResponseEntity<Department> addDepartment(@Valid @RequestBody Department department) {
         repository.save(department);
         return new ResponseEntity<>(department, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDepartment(@PathVariable("id") UUID uuid) {
+        Department department = repository.findById(uuid).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid Department ID: " + uuid));
+        repository.delete(department);
+        return new ResponseEntity("Department deleted with ID: " + uuid, HttpStatus.OK);
     }
 }
--- src/main/java/io/stahlferro/mangorhodes/controllers/RoomController.java
+++ src/main/java/io/stahlferro/mangorhodes/controllers/RoomController.java
@@ -1,14 +1,17 @@
 package io.stahlferro.mangorhodes.controllers;
 
+import io.stahlferro.mangorhodes.models.primary.Department;
 import io.stahlferro.mangorhodes.models.primary.Room;
 import io.stahlferro.mangorhodes.repositories.primary.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/room")
@@ -24,5 +27,13 @@ public class RoomController {
     public ResponseEntity<Room> addRoom(@Valid @RequestBody Room room) {
         repository.save(room);
         return new ResponseEntity<>(room, HttpStatus.CREATED);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteRoom(@PathVariable("id") UUID uuid) {
+        Room room = repository.findById(uuid).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid Room ID: " + uuid));
+        repository.delete(room);
+        return new ResponseEntity("Room deleted with ID: " + uuid, HttpStatus.OK);
     }
 }
